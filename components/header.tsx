1f2aab03bae372981ae7826bf698d1c7674f0a48
david emmerich <squasmn.withem@gmail.com>
2024-05-17 11:09:56 +0200

    testing features

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./navbar";
+
+const Header: React.FC = () => (
+  <header>
+    <Navbar />
+    {/* Other header content... */}
+  </header>
+);
+
+export default Header;
---
+++ components/navbar.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
+const Navbar: React.FC = () => (
+  <nav>
+    {/* Other navbar content... */}
+    <ModeToggle />
+  </nav>
+);
+
+export default Navbar;
